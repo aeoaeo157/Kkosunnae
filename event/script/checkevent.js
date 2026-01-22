0bb585b58338f676a1c74b621fb201cdafae79be
aeoaeo157 <perl33157@gmail.com>
2026-01-22 17:59:47 +0900

    26.1.22.3

--- event/script/checkevent.js
+++ event/script/checkevent.js
@@ -25,6 +25,28 @@ $(document).ready(function() {
 
 
 
+//top 스크롤
+
+/* --- Top 스크롤 버튼 기능 (이미지 버전) --- */
+$(document).ready(function() {
+    const $scrollBtn = $('#scrollTopBtn');
+
+    // 스크롤 위치 감지 (300px 이상 내려오면 보임)
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 300) {
+            $scrollBtn.fadeIn();
+        } else {
+            $scrollBtn.fadeOut();
+        }
+    });
+
+    // 버튼 클릭 시 최상단으로 부드럽게 이동
+    $scrollBtn.on('click', function() {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+});
+
 
 
 
--- infomation/script/infomation.js
+++ infomation/script/infomation.js
@@ -1,27 +1,4 @@
-// facility.js
-
-
-$(document).ready(function() {
-    $(window).on('scroll', function() {
-        $('.facility .box').each(function() {
-            var pos = $(this).offset().top;
-            var winTop = $(window).scrollTop();
-            var winHeight = $(window).height();
-
-            // 화면 하단에서 200px 정도 들어왔을 때 애니메이션 실행
-            if (pos < winTop + winHeight - 200) {
-                $(this).addClass('on');
-            }
-        });
-    });
-
-    // 페이지 로드 시 스크롤 위치를 한 번 체크
-    $(window).trigger('scroll');
-});
-
-
-
-
+//infomation.js
 
 
 //top 스크롤
@@ -45,6 +22,9 @@ $(document).ready(function() {
         return false;
     });
 });
+
+
+
 
 
 
